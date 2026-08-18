0a5265a7fa3d18c1feaa43f5fca9eb5434c8b6ae
rhaynermartins <rhayner2005@hotmail.com>
2026-08-18 20:38:51 -0300

    study: update Java study exercise

--- Exercicios/Labs/Atividades.java
+++ Exercicios/Labs/Atividades.java
@@ -2,7 +2,7 @@ public class Atividades {
 
     public static void main(String[] args) {
         System.out.println("Exercicio de estudo gerado em: 2026-08-18 20:38:50");
-        System.out.println("Numero base: 9134");
+        System.out.println("Numero base: 12094");
 
         int[] valores = {5, 3, 8, 1, 2};
 
